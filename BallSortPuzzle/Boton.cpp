3a9580e385d65cc0e56633b48dc7c37315a4a575
JARC02 <josue.retana.cespedes@est.una.ac.cr>
2020-09-19 17:55:46 -0600

    nuevo

--- BallSortPuzzle/Boton.cpp
+++ BallSortPuzzle/Boton.cpp
@@ -5,14 +5,6 @@ Boton::Boton(float posx, float posy, float width, float heigth, Color color, Str
 	btn = new RectangleShape({ width, heigth });
 	btn->setFillColor(color);
 	btn->setPosition(posx, posy);
-	/*if (!fuente.loadFromFile("Fonts/samosan.ttf")) {
-		cout<< "Fallo ";
-	}*/
-	txt_btn.setString(nombre);
-	txt_btn.setFont(fuente);
-	txt_btn.setPosition(posx, posy);
-	txt_btn.setCharacterSize(10);
-	txt_btn.setFillColor(Color::Black);
 }
 
 RectangleShape* Boton::getRect() {
